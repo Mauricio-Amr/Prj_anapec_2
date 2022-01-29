6ae0e5495b29dc7f1ee639abe11634769a4a400a
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-01-28 22:49:52 -0300

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    app.use(cors());
+    app.use(cors(origin));
     next();
 });
     
