c66c0f21a618989cf0ce0004645085317a2165b7
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-01-28 23:00:07 -0300

    teste

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const PORT = process.env.PORT
 
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Origin','https://anapec2.herokuapp.com')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    app.use(cors(origin));
+    app.use(cors());
     next();
 });
     
